7edc3679edffdba78e73540126eff1cc6e52c82c
SangWoo Shin <ssw522b@naver.com>
2021-11-17 15:36:42 +0900

    SideBarController(use open library)

---
+++ Chapter04-SideBar/Chapter04-SideBar/Chapter04-SideBar-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+//브릿징 헤더 파일 : 오브젝트-C에서 정의된 객체를 스위프트 아키텍처가 참좔 수 있도록 중간에 다리를 놓아주는 역할
+//@임의로 생성하는 방법@
+//1. 빈 헤더파일 하나 생성
+//2. 프로젝트 메타 정보가서 Build Settings탭에서 Swift Compiler - General -> OBjective-C Bridging에 브릿징 헤더 파일명 직접 입력
+
+#import "SWRevealViewController.h"
